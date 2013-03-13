aa84b45a2f9f6f09f0fa9c711b9e25e6f10101fd
Simon Menke <simon.menke@gmail.com>
2013-03-13 17:16:47 +0100

    UPdated the pages resource

---
+++ lib/lalala/admin/pages.rb
@@ -0,0 +1,17 @@
+if defined?(ActiveAdmin)
+  ActiveAdmin.register ApplicationPage, :as => 'Page' do
+
+    config.filters = false
+
+    index as: :tree_table do
+      selectable_column
+      id_column
+      column :title
+      default_actions
+    end
+
+    form do |f|
+      self.instance_exec(f, &f.object.form)
+    end
+  end
+end
--- lib/lalala/engine.rb
+++ lib/lalala/engine.rb
@@ -1,5 +1,9 @@
 class Lalala::Engine < Rails::Engine
 
+  initializer "lalala.active_admin.load_path" do
+    ActiveAdmin.application.load_paths.unshift File.expand_path('../admin', __FILE__)
+  end
+
   ::Sass::Engine::DEFAULT_OPTIONS[:load_paths] << File.expand_path("../../../app/assets/stylesheets", __FILE__)
 
 end
