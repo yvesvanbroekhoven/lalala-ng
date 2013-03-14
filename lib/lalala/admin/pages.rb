4787d14521da453f2ed242e91bdf7b14e791144c
Simon Menke <simon.menke@gmail.com>
2013-03-14 18:20:33 +0100

    Fixed some settings

--- lib/lalala/admin/pages.rb
+++ lib/lalala/admin/pages.rb
@@ -1,11 +1,9 @@
 if defined?(ActiveAdmin)
   ActiveAdmin.register ApplicationPage, :as => 'Page' do
 
-    config.filters        = false
-    config.paginator      = false
-    config.download_links = false
+    config.filters = false
 
-    index as: :tree_table do
+    index as: :tree_table, paginator: false, download_links: false do
       selectable_column
       id_column
       column :title
