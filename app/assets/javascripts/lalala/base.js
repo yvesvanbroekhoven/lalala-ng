ae6815bd03fa7c6d84f0cb28b6fc46818985e96e
Steven Vandevelde <icid.asset@gmail.com>
2013-03-27 15:04:38 +0100

    Calendar and some form css

--- app/assets/javascripts/lalala/base.js
+++ app/assets/javascripts/lalala/base.js
@@ -1,6 +1,8 @@
 //= require active_admin/base
+//= require_tree ./lib
 
 //= require_tree ../browser
-//= require_tree ./lib
 //= require_tree ./modules
+//= require_self
+
 require('lalala/modules/init');
--- app/assets/javascripts/lalala/modules/init.module.js
+++ app/assets/javascripts/lalala/modules/init.module.js
@@ -1,7 +1,10 @@
-var console = require('browser/console'),
-    editor  = require("lalala/modules/editor");
+var console  = require('browser/console'),
+    editor   = require('lalala/modules/editor'),
+    calendar = require('lalala/modules/calendar');
 
 $(function(){
-  $('select').chosen();
   editor.init();
+  calendar.init();
+
+  // $("select").chosen();
 });
