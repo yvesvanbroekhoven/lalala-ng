dfe605cd3f8c596355926dc02cedb2c93bdf83e7
Simon Menke <simon.menke@gmail.com>
2013-03-28 15:14:43 +0100

    Some minor fixes to the auto gem builder

--- lib/lalala/version.rb
+++ lib/lalala/version.rb
@@ -2,7 +2,7 @@ module Lalala
   VERSION = "4.0.0"
   BUILD   = "{{BUILD_NUMBER}}"
 
-  if BUILD != "{{BUILD_NUMBER}}"
+  if BUILD != ("{{BUILD_NUMBER" + "}}") # prevent sed replacement (see script/ci)
     BUILD_VERSION = "#{VERSION}.dev.#{BUILD}"
   else
     BUILD_VERSION = VERSION
