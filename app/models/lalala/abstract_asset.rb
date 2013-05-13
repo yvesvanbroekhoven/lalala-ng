083ab36254a49f6229677b70d3ffc985208723ef
Steven Vandevelde <icid.asset@gmail.com>
2013-05-13 16:43:24 +0200

    Include I18nTranslationsWriter::Writer into Lalala::AbstractAsset

--- app/models/lalala/abstract_asset.rb
+++ app/models/lalala/abstract_asset.rb
@@ -1,8 +1,11 @@
 class Lalala::AbstractAsset < ActiveRecord::Base
+  include Lalala::ExtActiveRecord::I18nTranslationsWriter::Writer
+
   self.abstract_class = true
   self.table_name = "assets"
 
-  attr_accessible :asset, :title, :caption
+  attr_accessible :asset, :title, :caption, :translations_writer
+
   translates :title, :caption
   Lalala::AbstractAsset::Translation.table_name = 'asset_translations'
 
