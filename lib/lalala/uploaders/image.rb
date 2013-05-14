a25479aa8cab8e694f2dc44acb71d017ab2f979c
Steven Vandevelde <icid.asset@gmail.com>
2013-05-14 14:06:59 +0200

    Do not include sprockets helpers in image uploader

--- lib/lalala/uploaders/image.rb
+++ lib/lalala/uploaders/image.rb
@@ -1,8 +1,6 @@
 class Lalala::Uploaders::Image < Lalala::Uploaders::File
 
   include CarrierWave::MiniMagick
-  include Sprockets::Helpers::RailsHelper
-  include Sprockets::Helpers::IsolatedHelper
 
   def extension_white_list
     %w(jpg jpeg gif png)
