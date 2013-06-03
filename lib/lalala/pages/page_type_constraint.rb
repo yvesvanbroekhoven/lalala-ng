550be74ea70d5c67b3543d57ac3e2925a31057b8
Simon Menke <simon.menke@gmail.com>
2013-06-03 10:16:30 +0200

    Better page routing

--- lib/lalala/pages/page_type_constraint.rb
+++ lib/lalala/pages/page_type_constraint.rb
@@ -1,16 +1,19 @@
 class Lalala::Pages::PageTypeConstraint
 
   def initialize(type)
-    unless Class === type
-      type  = type.to_s.classify
-      type += "Page" unless type.ends_with?("Page")
-      type  = type.constantize
+    type = "#{type}_page"
+    type = type.classify
+
+    while type.ends_with?("PagePage")
+      type = type[0..-5]
     end
 
+    type = type.constantize
     @type = type
   end
 
   def matches?(request)
+    return false if env["action_dispatch.exception"]
     request.env['lalala.page'] and @type === request.env['lalala.page']
   end
 
