e2b41bc303bbc031fdebfb7c45882fb9ead936e0
Hans Spooren <hans@mrhenry.be>
2013-11-22 12:40:48 +0100

    Updated titleBar, updated TableHeadColor and TableHead a color

--- test/dummy/app/admin/articles.rb
+++ test/dummy/app/admin/articles.rb
@@ -1,5 +1,17 @@
 ActiveAdmin.register Article do
 
+  index do
+    selectable_column
+    column :title, :sortable => :title do |m|
+      link_to m.title, edit_lalala_post_path(m)
+    end
+    column :created_at, :sortable => :created_at do |d|
+      d.created_at.strftime('%b %d, %Y')
+    end
+    default_actions
+  end
+
+
   form do |f|
     f.inputs do
       f.input :title
